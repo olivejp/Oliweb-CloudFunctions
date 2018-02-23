0c7b00b18be30f305695afd3b6c198a9f3ed1467
orlanth23 <orlanth23@hotmail.com>
2018-02-23 23:07:26 +1100

    Operational

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as fbfunctions from 'firebase-functions';
+
 const elasticsearch = require('elasticsearch');
 const _ = require('lodash');
 const request = require('request-promise');
@@ -8,14 +9,14 @@ exports.indexAnnonceToElastic = fbfunctions.database.ref('/annonces/{annonceId}/
     .onWrite(event => {
         const annonceData = event.data.val();
         const annonceId = event.params.annonceId;
-        
+
         console.log('Indexing annonce ', annonceId, annonceData);
 
         const elasticSearchConfig = fbfunctions.config().elasticsearch;
         const elasticSearchUrl = elasticSearchConfig.url + 'annonces/annonce/' + annonceId;
         const elasticSearchMethod = annonceData ? 'POST' : 'DELETE';
 
-        console.log('Body compiled ', annonceData, _.pick(annonceData));
+        console.log('Body compiled ', annonceData);
 
         const elasticsearchRequest = {
             method: elasticSearchMethod,
@@ -24,7 +25,7 @@ exports.indexAnnonceToElastic = fbfunctions.database.ref('/annonces/{annonceId}/
                 username: elasticSearchConfig.username,
                 password: elasticSearchConfig.password,
             },
-            body: _.pick(annonceData),
+            body: annonceData,
             json: true
         };
 
@@ -45,47 +46,53 @@ exports.observeRequest = fbfunctions.database.ref('/requests/{requestId}/')
         const requestId = event.params.requestId;
 
         // We want avoid infinite loop, so we continue only if results === null
-        if (requestData.results === null) {
+        console.log('Request ', requestId, requestData);
 
-            console.log('Request ', requestId, requestData);
+        const elasticSearchConfig = fbfunctions.config().elasticsearch;
 
-            const elasticSearchConfig = fbfunctions.config().elasticsearch;
+        // Récupération des paramètres de notre recherche
+        const pageNum = requestData.page;
+        const perPage = requestData.perPage;
+        const search_query = requestData.searchQuery;
 
-            // Définition d'un client elasticsearch
-            const client = new elasticsearch.Client({
-                host: [{
-                    host: elasticSearchConfig.url,
-                    auth: elasticSearchConfig.username + ':' + elasticSearchConfig.password,
-                }],
-                log: 'debug'
-            });
+        console.log(pageNum, perPage, search_query);
 
-
-            // Récupération des paramètres de notre recherche
-            const pageNum = requestData.page;
-            const perPage = requestData.perPage;
-            const search_query = requestData.searchQuery;
-
-            // Lancement de la recherche
-            client.search({
-                index: 'annonces',
-                type: 'annonce',
+        // Lancement de la recherche
+        const elasticSearchUrl = elasticSearchConfig.url + 'annonces/_search';
+        const elasticsearchRequest = {
+            method: 'POST',
+            uri: elasticSearchUrl,
+            auth: {
+                username: elasticSearchConfig.username,
+                password: elasticSearchConfig.password,
+            },
+            body: {
                 from: (pageNum - 1) * perPage,
                 size: perPage,
-                body: {
-                    query: {
-                        multi_match: {
-                            query: search_query,
-                            fields: '["titre^3","description"]'
-                        }
+                query: {
+                    multi_match: {
+                        query: search_query,
+                        fields: ['titre','description']
                     }
                 }
-            }).then(resp => {
-                // Récupération du résultat et écriture dans notre FirebaseDatabase
-                const hits = resp.hits.hits;
-                return event.data.child('results').ref.set(hits, a => {
-                    console.log('Insertion dans results échouée : ' + a.message);
-                });
-            }, reason => console.log(reason.message));
-        }
+            },
+            json: true
+        };
+
+
+        return request(elasticsearchRequest).then(resp => {
+            // Récupération du résultat et écriture dans notre FirebaseDatabase
+            const hits = resp.hits.hits;
+            console.log('Response',resp);
+            console.log("Here are the hits => " + hits._source);
+
+            if (hits !== null) {
+                event.data.ref.child('results').set(hits)
+                    .then(value => console.log('Insertion réussie'))
+                    .catch(a => console.log('Insertion dans results échouée : ' + a.message));
+            } else {
+                console.log('Rien a retourner')
+            }
+
+        }).catch(reason => console.log('Houla ca va pas du tout la !' + reason.message));
     });
