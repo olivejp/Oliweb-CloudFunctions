de3dec278f55eb3360ee266191f9e92837773c6b
2761oli <jean-paul.olive@opt.nc>
2018-07-25 17:29:02 +1100

    Feat : Nouvelle function pour envoyer des notifications.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -47,6 +47,75 @@ function indexation(annonce: any): Promise<any> {
     return request(elasticsearchRequest);
 }
 
+// TODO Terminer cette function
+// Cloud function permettant d'envoyer une notification
+// Dès que quelqu'un reçoit un message.
+exports.observeRequest = functions.database.ref('/messages/{chatId}/{messageId}')
+    .onCreate((snapshot, context) => {
+
+        // Récupération de la requête et de son Id
+        const messageData = snapshot.val();
+        const chatId = context.params.chatId;
+        const messageId = context.params.messageId;
+        const authorId = messageData.uidAuthor;
+
+        console.log('Message envoyé ', messageId, messageData);
+
+        // Récupération du chat
+        const db.ref('/chats/${chatId}').once('value')
+            .catch(reason => console.error(new Error(reason)))
+            .then(chatData => {
+
+                // Récupération du tableau des membres participants au chat
+                let mapMembers: Map<string, boolean> = new Map<string, boolean>();
+                Object.keys(chatData.members).forEach(key => {
+                    if (chatData.members[key] === true) {
+                        this.mapMembers.set(key, chatData.members[key]);
+                    }
+                });
+
+                // Déduction des receveurs de la notification (tous sauf l'auteur)
+                let receiverIds = [];
+                for (let memberId: string of mapMembers.keys()){
+                    if (memberId !== authorId) {
+                        receiverIds.push(memberId);
+                    }
+                }
+
+                // Récupération du token dans les paramètres des utilisateurs
+                // TODO voir la solution proposée ici https://stackoverflow.com/questions/39875243/promises-in-the-foreach-loop-typescript-2
+                const tokens = [];
+                function searchTokens(receiverIds): Promise<any>{
+                    let promiseArray: Array<any> = [];
+                    for(let userId: string of receiverIds) {
+                        promiseArray.push()
+                        const db.ref('/users/${userId}').once('value')
+                            .catch(reason => console.error(new Error(reason)))
+                            .then(user => {
+                                tokens.push(user.tokenDevice)
+                            });
+                    }
+                    return Promise.all(promiseArray);
+                }
+
+                searchTokens(receiverIds).then(value => {
+
+                });
+
+                // Notification details.
+                const payload = {
+                    notification: {
+                        title: 'You have a new follower!',
+                        body: '${follower.displayName} is now following you',
+                        icon: follower.photoURL
+                    }
+                };
+            });
+
+        return admin.messaging().sendToDevice(token, payload);
+
+    });
+
 // Function d'indexation des annonces
 exports.indexAnnonceToElastic = functions.database.ref('/annonces/{annonceId}/')
     .onWrite((data, context) => {
@@ -78,7 +147,7 @@ exports.indexAnnonceToElastic = functions.database.ref('/annonces/{annonceId}/')
             .catch(reason => console.error('Houla ca va pas du tout la !' + reason.message));
     });
 
-// TODO finir cette Cloud Function
+// TODO tester cette function
 // Cloud function qui permettra de supprimer l'index annonces sur ES
 // Relire toutes la DB sur Firebase Database et réindexer toutes les annonces
 exports.reindexElasticsearch = functions.https.onRequest((req, res) => {
